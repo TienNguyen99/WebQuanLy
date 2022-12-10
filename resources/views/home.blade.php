cb4fa9f51ae260b8d512ec8c71bc63fb85a926c2
TienNguyen99 <38741031+TienNguyen99@users.noreply.github.com>
2022-12-10 17:42:11 +0700

    1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@include('sweetalert::alert')
+
+@section('content')
+<!--                     @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif -->
+<div class="container">
+
+</div>
+@endsection
