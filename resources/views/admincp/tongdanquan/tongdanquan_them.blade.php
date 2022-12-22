b20ee23ae4c57c9686211bde2c13f6d420966f9a
TienNguyen99 <38741031+TienNguyen99@users.noreply.github.com>
2022-12-22 17:07:03 +0700

    7

---
+++ resources/views/admincp/tongdanquan/tongdanquan_them.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<section class="section">
+                    <div class="card">
+                        
+                        <div class="card-body">
+                            <table class="table table-striped" id="table1">
+                                <thead>
+                                    <tr>
+                                        <th>Tên</th>
+                                        <th>Giới tính</th>
+                                        <th>Số điện thoại</th>
+                                        <th>Nơi sinh</th>
+                                        <th>Status</th>
+                                        <th>Chức năng</th>
+                                        
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    <?php foreach ($list as $key => $show_tongdanquan): ?>
+                                    <tr>
+                                        <td>{{$show_tongdanquan->tenkhaisinh}}</td>
+                                        <td>{{$show_tongdanquan->gioitinh}}</td>
+                                        <td>{{$show_tongdanquan->sdt}}</td>
+                                        <td>{{$show_tongdanquan->quequan}}</td>
+                                        <td>
+                                            <span class="badge bg-success">Active</span>
+                                        </td>
+                                        <td>
+                                        	<button type="button" data-bs-toggle="modal"
+                    data-bs-target="#inlineForm" class="btn btn-default" >
+                    	<i class="bi bi-clipboard-check"></i>
+
+                    </button>
+
+                                        </td>
+                                        
+                                    </tr>	
+                                    <?php endforeach ?>
+                                    
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </section>
+
+
+
+@endsection
